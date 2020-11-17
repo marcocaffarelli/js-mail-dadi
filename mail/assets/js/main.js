97dbc08b28abdbfaa940dbb6a90ee6ebb6239f28
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-17 15:58:34 +0100

    Creato un form con input email e button
    Creato un form con input email e button dove ho collegato il file js

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -1,33 +1,45 @@
-var mail = prompt("Inserisci la tua e-mail");
+// var mail = prompt("Inserisci la tua e-mail");
 // // console.log("mail");
+var btnConferma = document.getElementById("conferma_mail");
+btnConferma.addEventListener('click', function() {
 
-var listMail = ["Andrea@booleanmail.it","Antonio@booleanmail.it","Alfredo@booleanmail.it","Alberto@booleanmail.it","Alex@booleanmail.it","Arianna@booleanmail.it","Assunta@booleanmail.it","Aristide@booleanmail.it","Amerigo@booleanmail.it","Asia@booleanmail.it","Ania@booleanmail.it","Alessandro@booleanmail.it"]
+   var mailUtente = document.getElementById("mail_in")
+   var verificaMail = document.getElementById("verifica");
+   var verificata
 
-// console.log(listMail[0]);
-// console.log(listMail[1]);
-// console.log(listMail[2]);
-// console.log(listMail[3]);
-// console.log(listMail[4]);
-// console.log(listMail[5]);
-// console.log(listMail[6]);
-// console.log(listMail[7]);
-// console.log(listMail[8]);
-// console.log(listMail[9]);
-// console.log(listMail[10]);
-// console.log(listMail[11]);
 
- // var invitato = listMail[1];
- // console.log(invitato);
+   var mail = mailUtente.value;
+   var listMail = ["Andrea@booleanmail.it","Antonio@booleanmail.it","Alfredo@booleanmail.it","Alberto@booleanmail.it","Alex@booleanmail.it","Arianna@booleanmail.it","Assunta@booleanmail.it","Aristide@booleanmail.it","Amerigo@booleanmail.it","Asia@booleanmail.it","Ania@booleanmail.it","Alessandro@booleanmail.it"]
 
-for (var i = 0; i < listMail.length; i++){
-  console.log(listMail[i]);
+   // console.log(listMail[0]);
+   // console.log(listMail[1]);
+   // console.log(listMail[2]);
+   // console.log(listMail[3]);
+   // console.log(listMail[4]);
+   // console.log(listMail[5]);
+   // console.log(listMail[6]);
+   // console.log(listMail[7]);
+   // console.log(listMail[8]);
+   // console.log(listMail[9]);
+   // console.log(listMail[10]);
+   // console.log(listMail[11]);
 
-var invitato = listMail[i];
+   // var invitato = listMail[1];
+   // console.log(invitato);
 
-if (mail == invitato){
-  console.log("Sei nella lista Benvenuto");
-}else{
-  console.log("Mi dispiace non sei nella lista non puoi entrare")
-}
+  for (var i = 0; i < listMail.length; i++){
+    // console.log(listMail[i]);
+  var invitato = listMail[i];
 
-}
+  if (mail == invitato){
+    // console.log("Sei nella lista Benvenuto");
+    verificata = "Sei nella lista Benvenuto"
+  }else {
+    // console.log("Mi dispiace non sei nella lista non puoi entrare")
+    verificata = "Mi dispiace non sei nella lista non puoi entrare"
+
+  }
+
+  }
+  verificaMail.innerHTML = verificata
+  });
