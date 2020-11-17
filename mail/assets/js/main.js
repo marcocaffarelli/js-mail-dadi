2e0ccaf95d0c4cbfbe2e43217bdfb73fe2e8c97a
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-17 15:01:05 +0100

    Creato un ciclo con le e-mail valide
    Creato un ciclo con le e-mail valide

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -3,25 +3,31 @@ var mail = prompt("Inserisci la tua e-mail");
 
 var listMail = ["Andrea@booleanmail.it","Antonio@booleanmail.it","Alfredo@booleanmail.it","Alberto@booleanmail.it","Alex@booleanmail.it","Arianna@booleanmail.it","Assunta@booleanmail.it","Aristide@booleanmail.it","Amerigo@booleanmail.it","Asia@booleanmail.it","Ania@booleanmail.it","Alessandro@booleanmail.it"]
 
-console.log(listMail[0]);
-console.log(listMail[1]);
-console.log(listMail[2]);
-console.log(listMail[3]);
-console.log(listMail[4]);
-console.log(listMail[5]);
-console.log(listMail[6]);
-console.log(listMail[7]);
-console.log(listMail[8]);
-console.log(listMail[9]);
-console.log(listMail[10]);
-console.log(listMail[11]);
- var invitato = listMail[1];
- console.log(invitato);
+// console.log(listMail[0]);
+// console.log(listMail[1]);
+// console.log(listMail[2]);
+// console.log(listMail[3]);
+// console.log(listMail[4]);
+// console.log(listMail[5]);
+// console.log(listMail[6]);
+// console.log(listMail[7]);
+// console.log(listMail[8]);
+// console.log(listMail[9]);
+// console.log(listMail[10]);
+// console.log(listMail[11]);
 
+ // var invitato = listMail[1];
+ // console.log(invitato);
 
+for (var i = 0; i < listMail.length; i++){
+  console.log(listMail[i]);
 
-if (mail == listMail){
+var invitato = listMail[i];
+
+if (mail == invitato){
   console.log("Sei nella lista Benvenuto");
 }else{
   console.log("Mi dispiace non sei nella lista non puoi entrare")
+}
+
 }
