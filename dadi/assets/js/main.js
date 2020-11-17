5b3e019c70e4cb49e0370d4a5ddc074076e8089d
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-17 17:15:19 +0100

    Completato esercizo Dadi
    Completato esercizo Dadi

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -1,14 +1,23 @@
 var btnPlay = document.getElementById("btn_play");
-
+var risultato = document.getElementById("Risultato_dadi");
+var numeroUtente = document.getElementById("numero_utente");
+var numeroComputer = document.getElementById("numero_computer");
 btnPlay.addEventListener('click', function() {
   var numeroDadoUtente = Math.floor(Math.random() *5) + 1;
+  // console.log(numeroDadoUtente);
+  numeroUtente.innerHTML = numeroDadoUtente + " Il Tuo Numero"
   var numeroDadoComputer = Math.floor(Math.random() *5) + 1;
-
+  // console.log(numeroDadoComputer);
+  numeroComputer.innerHTML = numeroDadoComputer + " Numero Computer"
   if (numeroDadoUtente > numeroDadoComputer) {
-    console.log("Complimenti hai visto");
+    // console.log( "Complimenti hai visto");
+    messaggioRisultato = "Complimenti hai visto"
   } else if(numeroDadoUtente < numeroDadoComputer){
-    console.log("Mi dispiace hai perso");
+    // console.log("Mi dispiace hai perso");
+    messaggioRisultato = "Mi dispiace hai perso"
   } else{
-    console.log("Un pareggio");
+    // console.log("Un pareggio");
+    messaggioRisultato = "Un pareggio"
   }
+  risultato.innerHTML = messaggioRisultato
 });
