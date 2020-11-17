85e59727bc1a702e26369a1a99f4d86f9d62cb5c
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-17 14:41:37 +0100

    Aggiunta lista e-mail
    Aggiunta lista e-mail e verificata

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -1,2 +1,27 @@
 var mail = prompt("Inserisci la tua e-mail");
-// console.log("mail");
+// // console.log("mail");
+
+var listMail = ["Andrea@booleanmail.it","Antonio@booleanmail.it","Alfredo@booleanmail.it","Alberto@booleanmail.it","Alex@booleanmail.it","Arianna@booleanmail.it","Assunta@booleanmail.it","Aristide@booleanmail.it","Amerigo@booleanmail.it","Asia@booleanmail.it","Ania@booleanmail.it","Alessandro@booleanmail.it"]
+
+console.log(listMail[0]);
+console.log(listMail[1]);
+console.log(listMail[2]);
+console.log(listMail[3]);
+console.log(listMail[4]);
+console.log(listMail[5]);
+console.log(listMail[6]);
+console.log(listMail[7]);
+console.log(listMail[8]);
+console.log(listMail[9]);
+console.log(listMail[10]);
+console.log(listMail[11]);
+ var invitato = listMail[1];
+ console.log(invitato);
+
+
+
+if (mail == listMail){
+  console.log("Sei nella lista Benvenuto");
+}else{
+  console.log("Mi dispiace non sei nella lista non puoi entrare")
+}
